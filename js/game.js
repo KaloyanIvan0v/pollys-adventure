347de9e96f2f364f3ecc9063a5c42fd98fb32939
Kaloyan Ivanov <kaloyanivanov@web.de>
2024-09-18 18:26:39 +0200

    endscreen-fix

--- js/game.js
+++ js/game.js
@@ -1,97 +1,98 @@
-let canvas;
-let world;
-let mouse;
-let keyboard = new Keyboard();
-let muteSound = true;
-let soundVolumeGame = 1;
-let soundVolumeGUI = 1;
-let gamePaused = false;
-let gameRuns = false;
-let holdWorld = false;
-let activeSounds = [];
-let gameMenu = false;
-let gameLoopTicks = 0;
-let fullscreen = false;
-let lastHoveredButton;
-let actualDevicePc = true;
-let renderSpeed = 60;
-let gameWon = false;
+let canvas
+let world
+let mouse
+let keyboard = new Keyboard()
+let muteSound = true
+let soundVolumeGame = 1
+let soundVolumeGUI = 1
+let gamePaused = false
+let gameRuns = false
+let holdWorld = false
+let activeSounds = []
+let gameMenu = false
+let gameLoopTicks = 0
+let fullscreen = false
+let lastHoveredButton
+let actualDevicePc = true
+let renderSpeed = 60
+let gameWon = false
 
 /**
  * Initialization function to set up the game environment.
  */
 function init() {
-  refreshCanvas();
-  loadStartScreen();
-  screenInterval();
-  touchEvents();
-  initClickEventForFullscreen();
-  initTouchListener();
-  screenResizeListener();
+  refreshCanvas()
+  loadStartScreen()
+  screenInterval()
+  touchEvents()
+  initClickEventForFullscreen()
+  initTouchListener()
+  screenResizeListener()
 }
 
 /**
  * Initialize click event listener for fullscreen instruction.
  */
 function initClickEventForFullscreen() {
-  document.getElementById("id-fullscreen-instruction").addEventListener("click", playInFullscreen);
+  document.getElementById('id-fullscreen-instruction').addEventListener('click', playInFullscreen)
 }
 
 /**
  * Function to toggle fullscreen mode.
  */
 function toggleFullscreen() {
-  !fullscreen ? enterFullScreen() : exitFullScreen();
+  !fullscreen ? enterFullScreen() : exitFullScreen()
   setTimeout(() => {
-    world.buttonResizeUpdate();
-  }, 20);
+    world.buttonResizeUpdate()
+  }, 20)
 }
 
 /**
  * Function to restart the game.
  */
 function playAgain() {
-  holdWorld = false;
-  reload();
+  holdWorld = false
+  reload()
 }
 
 /**
  * Function to go back to the home screen.
  */
 function backHome() {
-  world.stopGameLoop();
-  level1 = new Level(3, 1);
-  loadStartScreen();
-  holdWorld = false;
-  muteSound = true;
+  world.stopGameLoop()
+  level1 = new Level(3, 1)
+  loadStartScreen()
+  holdWorld = false
+  muteSound = true
+  gameWon = false
 }
 
 /**
  * Function to start the game.
  */
 function startGame() {
-  startScreen.stopLoop();
-  loadGame();
+  startScreen.stopLoop()
+  loadGame()
   setTimeout(() => {
-    muteSound = false;
-    gamePaused = true;
-  }, 200);
+    muteSound = false
+    gamePaused = true
+  }, 200)
 
-  screenOrientationListener();
+  screenOrientationListener()
 }
 
 /**
  * Function to reload the game.
  */
 function reload() {
-  world.restartGame();
+  world.restartGame()
 }
 
 /**
  * Function to toggle the game menu.
  */
 function toggleGameMenu() {
-  gameMenu = !gameMenu;
+  gameMenu = !gameMenu
 }
 
 /**
@@ -99,11 +100,11 @@ function toggleGameMenu() {
  */
 function toggleHowToPlay() {
   if (world.howToPlayShown) {
-    world.howToPlayShown = false;
-    gamePaused ? togglePlayPause() : null;
+    world.howToPlayShown = false
+    gamePaused ? togglePlayPause() : null
   } else {
-    world.howToPlayShown = true;
-    !gamePaused ? togglePlayPause() : null;
+    world.howToPlayShown = true
+    !gamePaused ? togglePlayPause() : null
   }
 }
 
@@ -111,26 +112,26 @@ function toggleHowToPlay() {
  * Function to load the start screen.
  */
 function loadStartScreen() {
-  gameRuns = false;
-  canvas = document.getElementById("canvas");
-  startScreen = new StartScreen(canvas, keyboard);
-  mouse = new Mouse(canvas);
+  gameRuns = false
+  canvas = document.getElementById('canvas')
+  startScreen = new StartScreen(canvas, keyboard)
+  mouse = new Mouse(canvas)
 }
 
 /**
  * Function to load the game.
  */
 function loadGame() {
-  canvas = document.getElementById("canvas");
-  world = new World(canvas, keyboard);
-  gameRuns = true;
+  canvas = document.getElementById('canvas')
+  world = new World(canvas, keyboard)
+  gameRuns = true
 }
 
 /**
  * Function to execute on every mouse click.
  */
 function executeOnEveryMouseClick() {
-  closeHowToPlayOnFreeClick();
+  closeHowToPlayOnFreeClick()
 }
 
 /**
@@ -139,10 +140,10 @@ function executeOnEveryMouseClick() {
 function executeOnEveryTouch() {
   if (world != undefined) {
     setTimeout(() => {
-      closeHowToPlayOnFreeClick();
-    }, 20);
+      closeHowToPlayOnFreeClick()
+    }, 20)
   } else {
-    closeHowToPlayOnFreeClick();
+    closeHowToPlayOnFreeClick()
   }
 }
 
@@ -156,7 +157,7 @@ function closeHowToPlayOnFreeClick() {
       !world.gameMenu.infoButton.isHovered(mouse.lastClick.x, mouse.lastClick.y) &&
       !world.gameMenu.infoButton.pressed
     ) {
-      toggleHowToPlay();
+      toggleHowToPlay()
     }
   }
 }
@@ -165,7 +166,7 @@ function closeHowToPlayOnFreeClick() {
  * Function to toggle sound on/off.
  */
 function toggleSound() {
-  muteSound = !muteSound;
+  muteSound = !muteSound
 }
 
 /**
@@ -174,39 +175,39 @@ function toggleSound() {
 function togglePlayPause() {
   setTimeout(() => {
     //this timeout is needed, because to set the sounds to mute the game loop needs to run.
-    gamePaused = !gamePaused;
-  }, 20);
-  !gamePaused && !muteSound ? toggleSound() : null;
-  gamePaused && muteSound ? toggleSound() : null;
+    gamePaused = !gamePaused
+  }, 20)
+  !gamePaused && !muteSound ? toggleSound() : null
+  gamePaused && muteSound ? toggleSound() : null
 }
 
 /**
  * Function to enter fullscreen mode.
  */
 function enterFullScreen() {
-  const canvas = document.getElementById("canvas");
+  const canvas = document.getElementById('canvas')
   if (canvas.requestFullscreen) {
-    fullscreen = true;
-    canvas.requestFullscreen();
+    fullscreen = true
+    canvas.requestFullscreen()
   } else if (canvas.mozRequestFullScreen) {
-    fullscreen = true;
-    canvas.mozRequestFullScreen();
+    fullscreen = true
+    canvas.mozRequestFullScreen()
   } else if (canvas.webkitRequestFullscreen) {
-    fullscreen = true;
-    canvas.webkitRequestFullscreen();
+    fullscreen = true
+    canvas.webkitRequestFullscreen()
   } else if (canvas.msRequestFullscreen) {
-    fullscreen = true;
-    canvas.msRequestFullscreen();
+    fullscreen = true
+    canvas.msRequestFullscreen()
   } else if (canvas.webkitEnterFullscreen) {
-    fullscreen = true;
-    canvas.webkitEnterFullscreen();
+    fullscreen = true
+    canvas.webkitEnterFullscreen()
   } else {
-    alert("Fullscreen mode is not supported on this device.");
-    fullscreen = true;
-    return;
+    alert('Fullscreen mode is not supported on this device.')
+    fullscreen = true
+    return
   }
   if (gamePaused) {
-    togglePlayPause();
+    togglePlayPause()
   }
 }
 
@@ -215,76 +216,76 @@ function enterFullScreen() {
  */
 function exitFullScreen() {
   if (document.exitFullscreen) {
-    document.exitFullscreen();
+    document.exitFullscreen()
   } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen();
+    document.mozCancelFullScreen()
   } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
+    document.webkitExitFullscreen()
   } else if (document.msExitFullscreen) {
-    document.msExitFullscreen();
+    document.msExitFullscreen()
   }
-  fullscreen = false;
-  !gamePaused ? togglePlayPause() : null;
+  fullscreen = false
+  !gamePaused ? togglePlayPause() : null
 }
 
 /**
  * Event listener for keydown events.
  */
-window.addEventListener("keydown", (e) => {
-  if (e.code === "KeyD") {
-    keyboard.RIGHT = true;
+window.addEventListener('keydown', (e) => {
+  if (e.code === 'KeyD') {
+    keyboard.RIGHT = true
   }
-  if (e.code === "KeyA") {
-    keyboard.LEFT = true;
+  if (e.code === 'KeyA') {
+    keyboard.LEFT = true
   }
-  if (e.code === "Space") {
-    keyboard.SPACE = true;
+  if (e.code === 'Space') {
+    keyboard.SPACE = true
   }
-  if (e.code === "KeyW") {
-    keyboard.UP = true;
+  if (e.code === 'KeyW') {
+    keyboard.UP = true
   }
-  if (e.code === "KeyT") {
-    keyboard.T = true;
+  if (e.code === 'KeyT') {
+    keyboard.T = true
   }
-  if (e.code === "KeyEsc") {
-    keyboard.ESC = true;
+  if (e.code === 'KeyEsc') {
+    keyboard.ESC = true
   }
-});
+})
 
 /**
  * Event listener for keyup events.
  */
-window.addEventListener("keyup", (e) => {
-  if (e.code === "KeyD") {
-    keyboard.RIGHT = false;
+window.addEventListener('keyup', (e) => {
+  if (e.code === 'KeyD') {
+    keyboard.RIGHT = false
   }
-  if (e.code === "KeyA") {
-    keyboard.LEFT = false;
+  if (e.code === 'KeyA') {
+    keyboard.LEFT = false
   }
-  if (e.code === "Space") {
-    keyboard.SPACE = false;
+  if (e.code === 'Space') {
+    keyboard.SPACE = false
   }
-  if (e.code === "KeyW") {
-    keyboard.UP = false;
+  if (e.code === 'KeyW') {
+    keyboard.UP = false
   }
-  if (e.code === "KeyT") {
-    keyboard.T = false;
+  if (e.code === 'KeyT') {
+    keyboard.T = false
   }
-  if (e.code === "KeyEsc") {
-    keyboard.ESC = false;
+  if (e.code === 'KeyEsc') {
+    keyboard.ESC = false
   }
-});
+})
 
 /**
  * Function to initialize touch event listeners.
  */
 function initTouchListener() {
-  window.addEventListener("touchstart", executeOnEveryTouch);
+  window.addEventListener('touchstart', executeOnEveryTouch)
 }
 
 /**
  * Function to execute when the device changes.
  */
 function executeOnDeviceChange() {
-  world != undefined ? world.buttonResizeUpdate() : null;
+  world != undefined ? world.buttonResizeUpdate() : null
 }
--- models/card.class.js
+++ models/card.class.js
@@ -1,8 +1,8 @@
 class Card extends ThrowableObject {
-  hitObject = false;
-  EXPLODE_ANIMATION = [];
+  hitObject = false
+  EXPLODE_ANIMATION = []
 
-  explodeSound = new Audio("/audio/objects/card-cannon/blub.mp3");
+  explodeSound = new Audio('/audio/objects/card-cannon/blub.mp3')
   /**
    * Loads paths to images for the card explosion animation into the EXPLODE_ANIMATION array.
    */
@@ -10,7 +10,7 @@ class Card extends ThrowableObject {
     for (let i = 53; i >= 0; i--) {
       i < 10
         ? this.EXPLODE_ANIMATION.push(`/img/animations/card-explosion/00${i}.png`)
-        : this.EXPLODE_ANIMATION.push(`/img/animations/card-explosion/0${i}.png`);
+        : this.EXPLODE_ANIMATION.push(`/img/animations/card-explosion/0${i}.png`)
     }
   }
 
@@ -23,27 +23,27 @@ class Card extends ThrowableObject {
    * @param {number} fallSpeed - Speed at which the card falls.
    */
   constructor(x, y, speedX, speedY, fallSpeed) {
-    super(x, y, speedX, speedY, fallSpeed);
-    this.loadExplodeImages();
-    this.loadImages(this.EXPLODE_ANIMATION);
-    this.loadImg("/img/objects/card/003-000.png");
-    this.width = 25;
-    this.height = 25;
+    super(x, y, speedX, speedY, fallSpeed)
+    this.loadExplodeImages()
+    this.loadImages(this.EXPLODE_ANIMATION)
+    this.loadImg('/img/objects/card/003-000.png')
+    this.width = 25
+    this.height = 25
   }
 
   /**
    * Main loop function for the Card's behavior.
    */
   objLoop() {
-    this.applyGravity(0.5);
-    this.throw();
+    this.applyGravity(0.5)
+    this.throw()
     if (this.hitObject) {
       if (this.animationStillRunning()) {
-        this.explodeAnimation();
-        this.playLastSound(this.explodeSound);
+        this.explodeAnimation()
+        this.playLastSound(this.explodeSound)
         setTimeout(() => {
-          this.alreadyDead = true;
-        }, 70);
+          this.alreadyDead = true
+        }, 70)
       }
     }
   }
@@ -54,38 +54,38 @@ class Card extends ThrowableObject {
    * @returns {boolean} - True if colliding with any item, false otherwise.
    */
   isCollidingWith(array) {
-    let collided = false;
+    let collided = false
     array.forEach((item) => {
       if (this.isColliding(item)) {
-        item.energy -= 100;
-        collided = true;
+        item.energy -= 100
+        collided = true
       }
-    });
-    return collided;
+    })
+    return collided
   }
 
   /**
    * Plays the card explosion animation.
    */
   explodeAnimation() {
-    this.playAnimation(this.EXPLODE_ANIMATION, 0.001);
+    this.playAnimation(this.EXPLODE_ANIMATION, 0.001)
   }
 
   checkIfCardCollidingWithEnemies() {
     world.level.enemies.forEach((enemy) => {
       if (this.isColliding(enemy) && this.harmful && !enemy.alreadyDead) {
-        const currentTime = Date.now();
+        const currentTime = Date.now()
         if (!enemy.lastHitTime || currentTime - enemy.lastHitTime >= 1000) {
-          this.hitObject = true;
-          enemy.energy -= 100;
+          this.hitObject = true
+          enemy.energy -= 100
           if (enemy.energy <= 0) {
-            enemy.energy = 0;
-            enemy.harmful = false;
+            enemy.energy = 0
+            enemy.harmful = false
           }
-          enemy.lastHitTime = currentTime;
+          enemy.lastHitTime = currentTime
         }
       }
-    });
+    })
   }
 
   /**
@@ -94,9 +94,9 @@ class Card extends ThrowableObject {
    */
   checkIfCollidingWithGround() {
     if (this instanceof Card && !this.isAboveGround()) {
-      this.hitObject = true;
+      this.hitObject = true
       if (this.alreadyDead) {
-        world.throwableObjects.splice(world.throwableObjects.indexOf(this), 1);
+        world.throwableObjects.splice(world.throwableObjects.indexOf(this), 1)
       }
     }
   }
